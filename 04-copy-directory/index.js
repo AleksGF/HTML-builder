a77857997e9c7cf507b958ff7553ee6d065a6eb7
AleksGF <aleksgf@mail.zp.ua>
2023-05-02 17:52:38 +0300

    feat: add 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+const copyDir = async () => {
+  try {
+    const srcDirPath = path.join(__dirname, 'files');
+    const srcDir = await fs.opendir(srcDirPath);
+
+    const destDirPath = await fs.mkdir('files-copy', { recursive: true });
+
+    if (!destDirPath) throw new Error('Error: Directory exist!');
+
+    for await (const dirent of srcDir) {
+      const fileName = dirent.name;
+      await fs.copyFile(path.join(srcDirPath, fileName), path.join(destDirPath, fileName));
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+copyDir();
